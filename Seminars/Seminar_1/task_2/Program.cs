232d42b59735f42f6458d19b8f82f76f145678db
OS <jwolga@yandex.ru>
2022-08-31 00:04:19 +0300

    added project task_2

---
+++ Seminars/Seminar_1/task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Input week day number");
+int week_day_number = int.Parse(Console.ReadLine());
+string week_day_name = null;
+
+switch(week_day_number)
+{
+    case 1:
+        week_day_name = "Monday";
+        break;
+    case 2:
+        week_day_name = "Tuesday";
+        break;
+    case 3:
+        week_day_name = "Wednesday";
+        break;
+    case 4:
+        week_day_name = "Thursday";
+        break;
+    case 5:
+        week_day_name = "Friday";
+        break;
+     case 6:
+        week_day_name = "Saturday";
+        break;
+     case 7:
+        week_day_name = "Sanday";
+        break;
+     default:
+        Console.WriteLine($"{week_day_number} is invalid");
+        return;
+
+}
+
+Console.WriteLine($"{week_day_number} is {week_day_name}");
