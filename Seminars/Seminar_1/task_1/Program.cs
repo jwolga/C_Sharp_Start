333f03361f5569d84a740e0f5a2fd3b18e29360e
OS <jwolga@yandex.ru>
2022-08-30 23:41:40 +0300

    added project task_1

--- Seminars/Seminar_1/task_1/Program.cs
+++ Seminars/Seminar_1/task_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//На вход 2 числа, является ли первое квадратом второго
+
+Console.WriteLine("Input the first number");
+var number_1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+var number_2 = int.Parse(Console.ReadLine());
+
+if (number_1 == number_2 * number_2)
+{
+    Console.WriteLine($"Number {number_1} is {number_2} ^2");
+}
+else
+{
+    Console.WriteLine($"Number {number_1} is not {number_2} ^2");
+}
